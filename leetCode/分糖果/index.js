0a5d9692af320a5b735077d5755170c059ada45d
qyfu <599769224@qq.com>
2024-07-05 00:27:14 +0800

    优化分糖果

--- leetCode/分糖果/index.js
+++ leetCode/分糖果/index.js
@@ -15,3 +15,18 @@ var distributeCandies = function(candyType) {
 };
 
 console.log(distributeCandies([6,6,6,6]));
+
+// 优化
+
+var distributeCandies11 = function(candyType) {
+    const types = new Set(candyType).size;
+    return Math.min(types, candyType.length / 2);
+};
+function distributeCandies22(candyType) {
+    // 使用 Set 来快速找出不重复的糖果种类
+    const types = new Set(candyType);
+    // 计算不重复种类的数量
+    const uniqueTypeCount = types.size;
+    // 比较并返回较小的值
+    return Math.min(uniqueTypeCount, Math.floor(candyType.length / 2));
+}
